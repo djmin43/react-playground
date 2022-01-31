8bf03eb994f44b2f3f5be4390016b1d34fd73fac
jay <dj.min43@gmail.com>
2022-01-31 23:05:35 +0900

    chore: usereducer

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,5 +1,25 @@
+import React, { useReducer, ReducerState } from "react";
+
 function App() {
-  return <></>;
+  const [count, setCount] = useReducer((count) => count + 1, 0);
+  const [information, setInformation] = useReducer(
+    (information: string, newInformation: string) =>
+      (information = newInformation),
+    "asdf"
+  );
+
+  return (
+    <>
+      <div>
+        <p>count</p>
+        {count}
+        <button onClick={setCount}>count</button>
+      </div>
+      <div>change information</div>
+      {information}
+      <button onClick={() => setInformation("hello")}>add information</button>
+    </>
+  );
 }
 
 export default App;
