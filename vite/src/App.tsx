a0511304f0baf56c37eeeb4184bcfa038515313f
jay <dj.min43@gmail.com>
2022-01-31 23:06:00 +0900

    chore: clean up

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,25 +1,7 @@
-import React, { useReducer, ReducerState } from "react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useReducer((count) => count + 1, 0);
-  const [information, setInformation] = useReducer(
-    (information: string, newInformation: string) =>
-      (information = newInformation),
-    "asdf"
-  );
-
-  return (
-    <>
-      <div>
-        <p>count</p>
-        {count}
-        <button onClick={setCount}>count</button>
-      </div>
-      <div>change information</div>
-      {information}
-      <button onClick={() => setInformation("hello")}>add information</button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
