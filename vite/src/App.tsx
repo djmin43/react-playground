9c65cedf37a8a14c05b98a7d5d5445d17ebfdf2e
jay <dj.min43@gmail.com>
2022-02-02 20:51:52 +0900

    trying usereducer

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 function App() {
-  return <></>;
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>add</button>
+      {state}
+    </>
+  );
+}
+
+function reducer(state: number, action: { type: string }) {
+  switch (action.type) {
+    case "INCREMENT":
+      console.log("inc", state);
+      return state + 1;
+    case "DECREMENT":
+      console.log("dec", state);
+      return state - 1;
+    default:
+      return state;
+  }
 }
 
 export default App;
