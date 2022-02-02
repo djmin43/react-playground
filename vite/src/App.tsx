9aea7e320ecd193a2203b10c93a34ed10bd32d09
jay <dj.min43@gmail.com>
2022-02-02 20:57:02 +0900

    chore: clean up;

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,27 +1,5 @@
-import React, { useReducer } from "react";
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, 0);
-
-  return (
-    <>
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>add</button>
-      {state}
-    </>
-  );
-}
-
-function reducer(state: number, action: { type: string }) {
-  switch (action.type) {
-    case "INCREMENT":
-      console.log("inc", state);
-      return state + 1;
-    case "DECREMENT":
-      console.log("dec", state);
-      return state - 1;
-    default:
-      return state;
-  }
+  return <></>;
 }
 
 export default App;
