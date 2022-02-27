8ec8fdd2822bdccfc80a3bc541cac4d94d470c06
jay <dj.min43@gmail.com>
2022-02-27 20:02:45 +0900

    rework

--- atomic-design/src/App.tsx
+++ atomic-design/src/App.tsx
@@ -2,18 +2,7 @@ import "./App.css";
 import styled from "@emotion/styled";
 
 function App() {
-  return (
-    <StyledContainer>
-      hello world. i'm figuring out atomic design
-    </StyledContainer>
-  );
+  return <div>hello world. i'm figuring out atomic design</div>;
 }
-
-const StyledContainer = styled.div`
-  width: 500px;
-  height: 500px;
-  border: 1px solid black;
-  border-radius: 4px;
-`;
 
 export default App;
