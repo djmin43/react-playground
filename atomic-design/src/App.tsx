c14480b8b36c46c25206380070e87203a2738e6c
jay <dj.min43@gmail.com>
2022-02-27 21:36:05 +0900

    test func

--- atomic-design/src/App.tsx
+++ atomic-design/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
 import styled from "@emotion/styled";
 
+type ObjectType = {
+  a: number;
+  b?: number;
+  c?: number;
+};
+
 function App() {
+  const testFunc = (obj: ObjectType) => {
+    return obj.a;
+  };
+
+  testFunc({ a: 1 });
+  testFunc({ a: 1, b: 2 });
+  testFunc({ a: 1, b: 2, c: 3 });
+
   return <div>hello world. i'm figuring out atomic design</div>;
 }
 
