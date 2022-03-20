fefa94a93fbbaa616b191261ae2db7355f303acf
jay <dj.min43@gmail.com>
2022-03-20 15:49:20 +0900

    .

--- nextjs12/pages/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  return new Response("let me go home!");
-}
--- nextjs12/pages/power.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PowerPage = () => {
-  return (
-    <div>power</div>
-  )
-}
-
-export default PowerPage
