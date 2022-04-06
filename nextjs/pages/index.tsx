28f9419284bc6dc0b63245db0c26678e12691051
jay <dj.min43@gmail.com>
2022-04-06 12:06:38 +0900

    remove class name and 'asdf'

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>asdf</div>;
+  return <div ></div>;
 };
 
 export default Home;
