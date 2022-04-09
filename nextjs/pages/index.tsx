c0a02363dceaf9f6384fa745e4d256ed64dd2247
jay <dj.min43@gmail.com>
2022-04-09 22:49:07 +0900

    try out new package

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,14 +1,12 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import SayHello from "jaymin-design-system";
 
 const Home: NextPage = () => {
   const someConstant = 10;
 
   return (
     <div>
-      <h1>hello world</h1>
+      <SayHello name="lee woohyuk" />
     </div>
   );
 };
