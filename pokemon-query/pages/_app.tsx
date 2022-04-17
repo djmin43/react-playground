2f74073b8ebad9ee79863ead7ff87733314c1922
jay <dj.min43@gmail.com>
2022-04-17 16:30:52 +0900

    add hydration

--- pokemon-query/pages/_app.tsx
+++ pokemon-query/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "../styles.css";
 
@@ -8,8 +8,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
