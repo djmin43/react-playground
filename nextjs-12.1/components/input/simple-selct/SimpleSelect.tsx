77564af738db68b619930459844477fb1844eca8
jay <dj.min43@gmail.com>
2022-04-24 17:53:56 +0900

    add type for handle click outside

--- nextjs-12.1/components/input/simple-selct/SimpleSelect.tsx
+++ nextjs-12.1/components/input/simple-selct/SimpleSelect.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./SimpleSelect.module.css";
 
 interface SimpleSelectProps {
@@ -23,8 +23,27 @@ const SimpleSelect = ({
     setIsSelectOpen(!isSelectOpen);
   }
 
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: Event) {
+      const selectDiv = selectRef.current;
+      if (selectDiv && !selectDiv.contains(event.target as Node)) {
+        setIsSelectOpen(false);
+      }
+    }
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [selectRef]);
+
   return (
-    <div className={styles.container} onClick={handleSelectOpen}>
+    <div
+      className={styles.container}
+      onClick={handleSelectOpen}
+      ref={selectRef}
+    >
       <div className={`${styles.select} `}>{value}</div>
       <span className={`${styles.arrow} ${isSelectOpen && styles.activeArrow}`}>
         arrow
