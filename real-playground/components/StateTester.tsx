4fb307475d3778816175936be1fd94e2c62beca6
james <james.min@freedgrouptech.com>
2022-05-16 07:26:12 +0900

    reducer

--- real-playground/components/StateTester.tsx
+++ real-playground/components/StateTester.tsx
@@ -1,11 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 const StateTester = () => {
-  const [count, setCount] = useState<number>(0);
+  const [count, dispatch] = useReducer(countReducer, 0);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    dispatch({
+      type: "add",
+    });
+  }, []);
 
   return <div>{count}</div>;
 };
+
+function countReducer(count: any, action: any) {
+  switch (action.type) {
+    case "add": {
+      console.log(count);
+      return 2;
+    }
+    case "subtract": {
+      console.log("subtract");
+      return 3;
+    }
+    case "double": {
+      return 4;
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
 
 export default StateTester;
