a40bbdeff14427487832de360f41da083a6dc473
jay <dj.min43@gmail.com>
2022-05-31 20:25:28 +0900

    move simple select to a new page

---
+++ nextjs-12.1/pages/simple-select/SimpleSelectPage.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import SimpleSelect from "../../components/input/simple-selct";
+import styles from "../../styles/Home.module.css";
+import { itemList } from "./itemList";
+
+const SimpleSelectPage = () => {
+  const [selectIndex, setSelectIndex] = useState<number>(0);
+  const [count, setCount] = useState<number>(5);
+  const [lionSize, setLionSize] = useState<number>(24);
+
+  function handleOnChange(event: React.MouseEvent<HTMLSpanElement>): void {
+    const indexSelected = event.currentTarget.dataset.value as string;
+    setSelectIndex(+indexSelected);
+    const newValue = itemList[+indexSelected].value;
+    setCount(+newValue);
+  }
+
+  function handleRange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const newSize: number = +event.target.value;
+    setLionSize(newSize);
+  }
+
+  function getAnimals(animalCount: number): Array<"🦁"> {
+    return new Array(animalCount).fill("🦁");
+  }
+
+  return (
+    <div className={styles.mainPage}>
+      <p className={styles.title}>my fun weekend</p>
+      <SimpleSelect
+        itemSelectList={itemList}
+        index={selectIndex}
+        onChange={handleOnChange}
+      />
+      <input
+        className={styles.range}
+        type="range"
+        onChange={handleRange}
+        min="12"
+        max="128"
+        value={lionSize}
+      />
+      <div className={styles.lionBox}>
+        <span style={{ fontSize: lionSize + "px" }}>{getAnimals(count)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleSelectPage;
---
+++ nextjs-12.1/pages/simple-select/itemList.ts
@@ -0,0 +1,39 @@
+interface Item {
+  label: string;
+  value: number;
+}
+
+export const itemList: Item[] = [
+  {
+    label: "5개씩 보기",
+    value: 5,
+  },
+  {
+    label: "10개씩 보기",
+    value: 10,
+  },
+  {
+    label: "20개씩 보기",
+    value: 20,
+  },
+  {
+    label: "50개씩 보기",
+    value: 50,
+  },
+  {
+    label: "100개씩 보기",
+    value: 100,
+  },
+  {
+    label: "500개씩 보기",
+    value: 500,
+  },
+  {
+    label: "1000개씩 보기",
+    value: 1000,
+  },
+  {
+    label: "10000개씩 보기",
+    value: 10000,
+  },
+];
