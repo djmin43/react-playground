620eb9b63920751c2f323c15efa8f3e069dcae24
jay <dj.min43@gmail.com>
2022-06-01 17:08:40 +0900

    add use pokemon hook

--- nextjs-12.1/pages/pokemon/PokemonPage.tsx
+++ nextjs-12.1/pages/pokemon/PokemonPage.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 function PokemonPage() {
-  console.log("https://pokeapi.co/api/v2/pokemon-species/");
-  return <div>this is pokemon page</div>;
+  const { isLoading, isError, data, error } = usePokemon();
+
+  if (isLoading) {
+    return <div>this is loading</div>;
+  }
+
+  if (isError) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{data.name}</div>;
+}
+
+function usePokemon() {
+  return useQuery("pokemon", () =>
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon-species/2")
+      .then((res) => res.data)
+  );
 }
 
 export default PokemonPage;
