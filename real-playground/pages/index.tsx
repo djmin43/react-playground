bb9d3739872bf1270b3674be142a2940ece73afb
jay <dj.min43@gmail.com>
2022-06-03 09:46:12 +0900

    .

--- real-playground/pages/index.tsx
+++ real-playground/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
 import StateTester from "../components/StateTester";
+import styles from "./home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <section class={""}}>
+      <div>some-container</div>
+      <p>hello world</p>
       <StateTester />
-    </div>
+    </section>
   );
 };
 
