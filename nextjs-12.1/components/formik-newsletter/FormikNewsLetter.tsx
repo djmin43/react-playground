5e9fad0fed9153c33a979e61e9f5ee527107ba8e
james <james.min@freedgrouptech.com>
2022-06-05 13:43:24 +0900

    more validation

--- nextjs-12.1/components/formik-newsletter/FormikNewsLetter.tsx
+++ nextjs-12.1/components/formik-newsletter/FormikNewsLetter.tsx
@@ -1,31 +1,52 @@
-import React from "react";
-import { useFormik } from "formik";
+import React from 'react';
+import { useFormik } from 'formik';
 
 const SignupForm = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+    // Note that we have to initialize ALL of fields with values. These
+    // could come from props, but since we don’t want to prefill this form,
+    // we just use an empty string. If we don’t do this, React will yell
+    // at us.
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+            />
 
-      <button type="submit">Submit</button>
-    </form>
-  );
+            <label htmlFor="lastName">Last Name</label>
+            <input
+                id="lastName"
+                name="lastName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+            />
+
+            <label htmlFor="email">Email Address</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
+
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
-
 export default SignupForm;
