5b34bae8ccb2b6479efea72b40459c3e70c11fda
jay <dj.min43@gmail.com>
2022-06-08 07:23:32 +0900

    trying out usefield

--- nextjs-12.1/components/pokemon/Pokemon.tsx
+++ nextjs-12.1/components/pokemon/Pokemon.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { MainPageLayout } from "../../styles/pokemon/pokemon-styles";
+import { Field, Form, Formik, FormikProps, useField } from "formik";
+
+const MyTextInput = ({ label, ...props }: any) => {
+  const [field, meta, helpers] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 function Pokemon(): JSX.Element {
   return (
     <MainPageLayout>
-      <h1>위워크 좋지</h1>
+      <h1>hello</h1>
+      <Formik
+        initialValues={{
+          id: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <MyTextInput label="id" name="id" type="text" placeholder="id" />
+        </Form>
+      </Formik>
     </MainPageLayout>
   );
 }
