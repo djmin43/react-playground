ff67206f6a9b494a8f1ef5544457551a79c1fd78
jay <dj.min43@gmail.com>
2022-06-08 06:52:41 +0900

    wework good

--- nextjs-12.1/components/pokemon/Pokemon.tsx
+++ nextjs-12.1/components/pokemon/Pokemon.tsx
@@ -4,7 +4,7 @@ import { MainPageLayout } from "../../styles/pokemon/pokemon-styles";
 function Pokemon(): JSX.Element {
   return (
     <MainPageLayout>
-      <h1>hello world</h1>
+      <h1>위워크 좋지</h1>
     </MainPageLayout>
   );
 }
