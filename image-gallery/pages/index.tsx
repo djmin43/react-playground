fc19aaa012d9d3a91c2e74085ff0f7c60def7d1c
Jay Min <dj.min43@gmail.com>
2022-07-11 07:31:01 +0900

    add loading

--- image-gallery/pages/index.tsx
+++ image-gallery/pages/index.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import Image from "next/image";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Gallery() {
   return (
@@ -11,6 +16,8 @@ export default function Gallery() {
 }
 
 function BlurImage() {
+  const [isLoading, setLoading] = useState(true);
+
   return (
     <a href="#" className="group">
       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
@@ -19,7 +26,13 @@ function BlurImage() {
           alt=""
           layout="fill"
           objectFit="cover"
-          className="group-hover:opacity-75"
+          className={classNames(
+            "group-hover:opacity-75 duration-700 ease-in-out",
+            isLoading
+              ? "grayscale blur-2xl scale-110"
+              : "grayscale-0 blur-0 scale-100"
+          )}
+          onLoadingComplete={() => setLoading(false)}
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">Jay Min</h3>
