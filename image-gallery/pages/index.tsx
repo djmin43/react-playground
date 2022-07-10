17a05b078250d6c30fa2c6e495f00b508c871a11
Jay Min <dj.min43@gmail.com>
2022-07-11 07:17:22 +0900

    use nextjs image comp

--- image-gallery/pages/index.tsx
+++ image-gallery/pages/index.tsx
@@ -1,20 +1,24 @@
+import Image from "next/image";
+
 export default function Gallery() {
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid gird-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <Image />
+        <BlurImage />
       </div>
     </div>
   );
 }
 
-function Image() {
+function BlurImage() {
   return (
     <a href="#" className="group">
       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
-        <img
+        <Image
           src="https://bit.ly/placeholder-img"
           alt=""
+          layout="fill"
+          objectFit="cover"
           className="group-hover:opacity-75"
         />
       </div>
