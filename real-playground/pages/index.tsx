e09a70b2fe035cb3c26584ce34fafac7a7ba3f3d
jay <dj.min43@gmail.com>
2022-07-10 19:48:28 +0900

    remove

--- real-playground/pages/index.tsx
+++ real-playground/pages/index.tsx
@@ -1,23 +1,8 @@
 import type { NextPage } from "next";
-import Card from "../components/Card";
-
-const tree = {
-  id: "1",
-  name: "project",
-  child: {
-    id: "2",
-    name: "first",
-    child: {
-      id: "3",
-      name: "second",
-    },
-  },
-};
 
 const Home: NextPage = () => {
   return (
     <section>
-      <Card tree={tree} />
       <p>hello world</p>
     </section>
   );
