010fbf53cb1bc8d42dba7aa493925dec8240a680
jay <dj.min43@gmail.com>
2022-07-30 18:35:04 +0900

    setting up

---
+++ image-gallery/pages/csr.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Csr = () => {
+  return <div></div>;
+};
+
+export default Csr;
--- image-gallery/pages/index.tsx
+++ image-gallery/pages/index.tsx
@@ -19,18 +19,6 @@ export async function getServerSideProps() {
   };
 }
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
-type Image = {
-  id: number;
-  href: string;
-  imageSrc: string;
-  name: string;
-  username: string;
-};
-
 interface GalleryProps {
   images: Image[];
 }
@@ -44,31 +32,5 @@ export default function Gallery({ images }: GalleryProps) {
         ))}
       </div>
     </div>
-  );
-}
-
-function BlurImage({ image }: { image: Image }) {
-  const [isLoading, setLoading] = useState(true);
-
-  return (
-    <a href={image.href} className="group">
-      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
-        <Image
-          src={image.imageSrc}
-          alt=""
-          layout="fill"
-          objectFit="cover"
-          className={classNames(
-            "group-hover:opacity-75 duration-700 ease-in-out",
-            isLoading
-              ? "grayscale blur-2xl scale-110"
-              : "grayscale-0 blur-0 scale-100"
-          )}
-          onLoadingComplete={() => setLoading(false)}
-        />
-      </div>
-      <h3 className="mt-4 text-sm text-gray-700">Jay Min</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@djmin43</p>
-    </a>
   );
 }
