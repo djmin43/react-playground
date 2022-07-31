98b807758684b4a88324d1cb0207af61149ce3d4
jay <dj.min43@gmail.com>
2022-07-31 20:02:03 +0900

    small styling

--- image-gallery/pages/index.tsx
+++ image-gallery/pages/index.tsx
@@ -3,23 +3,25 @@ import Link from "next/link";
 
 const Index = () => {
   return (
-    <ul>
-      <li>
-        <Link href="/csr">
-          <a>CSR</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/ssr">
-          <a>SSR</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/ssg">
-          <a>SSG</a>
-        </Link>
-      </li>
-    </ul>
+    <main className="h-screen flex items-center justify-center">
+      <ol>
+        <li>
+          <Link href="/csr">
+            <a className="hover:text-red-700">1. CSR</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/ssr">
+            <a className="hover:text-red-700">2. SSR</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/ssg">
+            <a className="hover:text-red-700">3. SSG</a>
+          </Link>
+        </li>
+      </ol>
+    </main>
   );
 };
 
