dcfab50d10ed17d7cfcdbfb6028c9049e867b360
Jay Min <dj.min43@gmail.com>
2022-08-03 07:31:53 +0900

    tutorial

---
+++ tutorial-n/src/pages/about.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid aperiam culpa, deleniti dolor ea enim ex exercitationem, impedit
+        incidunt itaque maiores neque nihil nisi porro quidem, ullam vel
+        voluptas.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid aperiam culpa, deleniti dolor ea enim ex exercitationem, impedit
+        incidunt itaque maiores neque nihil nisi porro quidem, ullam vel
+        voluptas.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid aperiam culpa, deleniti dolor ea enim ex exercitationem, impedit
+        incidunt itaque maiores neque nihil nisi porro quidem, ullam vel
+        voluptas.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ tutorial-n/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX designer & web developer based in Gimpo</p>
+      </div>
+    </section>
+  );
+};
+
+export default Index;
