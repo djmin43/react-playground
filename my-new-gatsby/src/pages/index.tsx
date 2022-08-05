b372236658905ad8f5502424ba5f2363720887b2
jay <dj.min43@gmail.com>
2022-08-05 15:10:41 +0900

    new with yarn

---
+++ my-new-gatsby/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+const IndexPage = () => {
+  return <main>hello world</main>;
+};
+
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Home Page</title>;
