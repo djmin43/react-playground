58c31010d87929cf9d4281b26aaa52d8bb366130
jay <dj.min43@gmail.com>
2022-08-05 15:35:08 +0900

    image

--- my-new-gatsby/src/pages/index.tsx
+++ my-new-gatsby/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
 import type { HeadFC } from "gatsby";
+import {StaticImage} from "gatsby-plugin-image";
+
+
+
 const IndexPage = () => {
-  return <main>hello world</main>;
+  return <main>
+    <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+    />
+
+  </main>;
 };
 
 export default IndexPage;
