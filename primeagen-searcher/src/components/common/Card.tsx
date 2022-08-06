77747efb394e458d34abf25b076f652601610643
jay <dj.min43@gmail.com>
2022-08-06 20:30:33 +0900

    static image

--- primeagen-searcher/src/components/common/Card.tsx
+++ primeagen-searcher/src/components/common/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { styled } from "@stitches/react";
 import { IProgrammer } from "../../types/programmer";
-import Img from "gatsby-image";
+import { StaticImage } from "gatsby-plugin-image";
 
 interface CardProps {
   programmerData: IProgrammer;
@@ -10,6 +10,10 @@ interface CardProps {
 const Card = ({ programmerData }: CardProps) => {
   return (
     <CardContainer>
+      <StaticImage
+        alt={programmerData.name}
+        src={programmerData.image.file.url}
+      />
       <h1>{programmerData.name}</h1>
       <p>{programmerData.description}</p>
       <p>{programmerData.mainLanguage}</p>
