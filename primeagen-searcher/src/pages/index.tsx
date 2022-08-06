007696d26448104c268692663f8183981569471e
jay <dj.min43@gmail.com>
2022-08-06 19:02:55 +0900

    remove conditional page

--- primeagen-searcher/src/pages/index.tsx
+++ primeagen-searcher/src/pages/index.tsx
@@ -1,10 +1,7 @@
-import * as React from "react"
+import * as React from "react";
 
 const IndexPage = () => {
-  return (
-      <div>hello this is primeagen</div>
-  )
-}
+  return <div>hello this is primeagen</div>;
+};
 
-export default IndexPage
-
+export default IndexPage;
