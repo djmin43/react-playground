42722aa5845015f7d76f8a3510e8d0a5f9b24607
jay <dj.min43@gmail.com>
2022-08-06 16:19:09 +0900

    use theme as plugin

---
+++ gatsby-theme-dev/example/.cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,12 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx?export=default" /* webpackChunkName: "component---src-pages-index-jsx" */)
+}
+
+
+
+exports.head = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
+  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx?export=head" /* webpackChunkName: "component---src-pages-index-jsxhead" */)
+}
+
---
+++ gatsby-theme-dev/example/.cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/dongjoonmin/Desktop/dev/react-playground/gatsby-theme-dev/example/.cache/dev-404-page.js")),
+  "component---src-pages-index-jsx": preferDefault(require("/Users/dongjoonmin/Desktop/dev/react-playground/gatsby-theme-dev/example/src/pages/index.jsx"))
+}
+
