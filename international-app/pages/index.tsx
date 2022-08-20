404c39908920c0d91ac36a14f28500ba13450f8d
jay <dj.min43@gmail.com>
2022-08-20 17:23:20 +0900

    apollo client makes things easier

--- international-app/pages/index.tsx
+++ international-app/pages/index.tsx
@@ -4,17 +4,22 @@ import fetchGraphQL from "../src/fetchGraphQL";
 import { useRouter } from "next/router";
 import { gql, useQuery } from "@apollo/client";
 
+const contentfulId = "3fbtgFlWlft8VWCoDLRuCC";
+
 const Home: NextPage = (props) => {
-  console.log(props);
-  const { data } = useQuery(QUERY);
-  console.log(data);
+  const { data } = useQuery(QUERY, {
+    variables: {
+      id: contentfulId,
+      locale: "en-US",
+    },
+  });
   return <div>hello world</div>;
 };
 
 const QUERY = gql`
   #  query ($id: String!, $locale: String!) {
-  query {
-    harmonyMain(id: "3fbtgFlWlft8VWCoDLRuCC", locale: "ko-KR") {
+  query HarmonyMain($id: String!, $locale: String) {
+    harmonyMain(id: $id, locale: $locale) {
       mainTitle
       mainDescription
       mainButton
