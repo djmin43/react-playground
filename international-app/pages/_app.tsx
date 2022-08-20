d1aa7c55e295584392df440cfa594268cd6b0536
jay <dj.min43@gmail.com>
2022-08-20 17:19:35 +0900

    query call with apollo works

--- international-app/pages/_app.tsx
+++ international-app/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
--- international-app/pages/index.tsx
+++ international-app/pages/index.tsx
@@ -2,30 +2,30 @@ import type { NextPage } from "next";
 import { useEffect } from "react";
 import fetchGraphQL from "../src/fetchGraphQL";
 import { useRouter } from "next/router";
+import { gql, useQuery } from "@apollo/client";
 
 const Home: NextPage = (props) => {
   console.log(props);
+  const { data } = useQuery(QUERY);
+  console.log(data);
   return <div>hello world</div>;
 };
 
-const query = `
+const QUERY = gql`
+  #  query ($id: String!, $locale: String!) {
   query {
-harmonyMainCollection(locale: "ko-KR") {
-    items {
-      description  {
-      json
-      }
+    harmonyMain(id: "3fbtgFlWlft8VWCoDLRuCC", locale: "ko-KR") {
+      mainTitle
+      mainDescription
+      mainButton
+      mainButtonLink
     }
   }
-}
-    `;
+`;
 
 export async function getStaticProps({ locale, locales }) {
-  const res = await fetchGraphQL(query);
-
   return {
     props: {
-      res,
       locale,
       locales,
     },
