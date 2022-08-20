c7a384d39deb22ac1792bf6952f990e00da3bb9b
jay <dj.min43@gmail.com>
2022-08-20 14:09:48 +0900

    next config n18n setting

--- international-app/next.config.js
+++ international-app/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en-US", "ko", "fr"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "en-US",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
