597b5893a2ea239e9af81bc3f1f7b13b2485f975
jay <dj.min43@gmail.com>
2022-09-10 20:44:09 +0900

    add transitions

--- xstate-testing/src/state-machine.ts
+++ xstate-testing/src/state-machine.ts
@@ -4,8 +4,21 @@ const promiseMachine = createMachine({
   id: "promise",
   initial: "pending",
   states: {
-    pending: {},
-    resolved: {},
-    rejected: {},
+    pending: {
+      on: {
+        RESOLVE: {
+          target: "resolved",
+        },
+        REJECT: {
+          target: "rejected",
+        },
+      },
+    },
+    resolved: {
+      type: "final",
+    },
+    rejected: {
+      type: "final",
+    },
   },
 });
