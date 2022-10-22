6bf89293aa6c36996208129e3db35094b2b23b11
jay <dj.min43@gmail.com>
2022-10-22 20:14:00 +0900

    java is the best

--- fabricjs/pages/index.tsx
+++ fabricjs/pages/index.tsx
@@ -7,17 +7,41 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     const newCanvas = new fabric.Canvas("canvas", {
-      height: 800,
+      height: 600,
       width: 800,
-      backgroundColor: "blue",
+      fireRightClick: true,
+      fireMiddleClick: true,
+      stopContextMenu: true,
+      backgroundColor: undefined,
+      backgroundImage: undefined,
+      selection: true,
     });
-    console.log(newCanvas);
-    // setCanvas(newCanvas);
+    const circle = new fabric.Circle({
+      radius: 100,
+      fill: "#eef",
+      scaleY: 0.5,
+      originX: "center",
+      originY: "center",
+    });
+
+    const text = new fabric.Text("java is the best", {
+      fontSize: 30,
+      originX: "center",
+      originY: "center",
+    });
+
+    const group = new fabric.Group([circle, text], {
+      left: 150,
+      top: 100,
+      angle: -10,
+    });
+
+    newCanvas.add(group);
   }, []);
 
   return (
     <div>
-      <h1>Fabric.js on React - fabric.Canvas('...')</h1>
+      <h1>GTO</h1>
       <canvas id="canvas" />
     </div>
   );
