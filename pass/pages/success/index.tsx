199a6a4a3a163ca561e10100ddbc84093755580b
Jay Min <dj.min43@gmail.com>
2022-10-28 10:17:13 +0900

    done

---
+++ pass/pages/success/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuccessPage = () => {
+  return <div>축하합니다</div>;
+};
+
+export default SuccessPage;
--- pass/pages/validate-user/index.tsx
+++ pass/pages/validate-user/index.tsx
@@ -1,3 +1,4 @@
+import { router } from "next/client";
 import React, { useEffect } from "react";
 
 const code = "imp10391932";
@@ -15,15 +16,17 @@ function ValidateUserPage() {
         // pg: "PG사코드.{CPID}", //본인인증 설정이 2개이상 되어 있는 경우 필
         // merchant_uid: "ORD20180131-0000011",
         // 모바일환경에서 popup:false(기본값) 인 경우 필수
-        // m_redirect_url: "https://naver.com",
+        m_redirect_url: "https://naver.com",
         // PC환경에서는 popup 파라메터가 무시되고 항상 true 로 적용됨
-        popup: false,
+        popup: true,
       },
       // @ts-ignore
       (rsp) => {
         // callback
         if (rsp.success) {
           // 인증 성공 시 로직,
+          console.log("yes!");
+          router.push("/success");
         } else {
           console.log("fail");
           // 인증 실패 시 로직,
