3a091a9488f584fcdc319b02e316d91ab552b355
Jay Min <dj.min43@gmail.com>
2022-10-28 09:29:09 +0900

    trying out import

--- pass/pages/_app.tsx
+++ pass/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        type="text/javascript"
+        src="https://code.jquery.com/jquery-3.6.1.min.js"
+        strategy={"beforeInteractive"}
+      />
+      <Script
+        type="text/javascript"
+        src="https://cdn.iamport.kr/js/iamport.payment-1.1.8.js"
+        strategy={"beforeInteractive"}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pass/pages/index.tsx
+++ pass/pages/index.tsx
@@ -1,11 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect } from "react";
+
+const code = "imp10391932";
+
+const apiKey = "5752633783078807";
 
 export default function Home() {
-  return (
-    <div >
-
-    </div>
-  )
+  useEffect(() => {
+    const IMP = window.IMP;
+    const container = IMP.init(code);
+    IMP.certification(
+      {
+        // param
+        // 주문 번호
+        merchant_uid: "ORD20180131-0000011",
+        // pg: "PG사코드.{CPID}", //본인인증 설정이 2개이상 되어 있는 경우 필
+        // merchant_uid: "ORD20180131-0000011",
+        // 모바일환경에서 popup:false(기본값) 인 경우 필수
+        // m_redirect_url: "https://naver.com",
+        // PC환경에서는 popup 파라메터가 무시되고 항상 true 로 적용됨
+        popup: true,
+      },
+      (rsp) => {
+        // callback
+        if (rsp.success) {
+          // 인증 성공 시 로직,
+        } else {
+          console.log("fail");
+          // 인증 실패 시 로직,
+        }
+      }
+    );
+  }, []);
+  return <div></div>;
 }
