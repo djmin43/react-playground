b990563a9e4bee07a421ae83c58dec7d6ecbd823
Jay Min <dj.min43@gmail.com>
2022-12-27 07:38:47 +0900

    creating more objects

--- fabricjs/pages/index.tsx
+++ fabricjs/pages/index.tsx
@@ -13,7 +13,24 @@ export default function Home() {
       height: 20,
       angle: 45,
     });
+    const circle = new fabric.Circle({
+      radius: 20,
+      fill: "green",
+      left: 100,
+      top: 100,
+    });
+    const triangle = new fabric.Triangle({
+      width: 20,
+      height: 30,
+      fill: "blue",
+      left: 50,
+      top: 50,
+    });
     canvas.add(rect);
+    canvas.add(circle);
+    canvas.add(triangle);
+    rect.set({ left: 20, top: 50 });
+    canvas.renderAll();
   }, []);
   return (
     <>
