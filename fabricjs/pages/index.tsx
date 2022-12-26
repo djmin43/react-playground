63d3bba012c891ce3e9914fa29ef2f23ab9f563a
Jay Min <dj.min43@gmail.com>
2022-12-27 07:27:55 +0900

    my first red rect

--- fabricjs/pages/index.tsx
+++ fabricjs/pages/index.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useRef } from "react";
+import { fabric } from "fabric";
 
 export default function Home() {
+  const canvasEl = useRef(null);
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasEl.current);
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: "red",
+      width: 20,
+      height: 20,
+    });
+    canvas.add(rect);
+  }, []);
   return (
     <>
-
+      <canvas width={"300"} height={"300"} ref={canvasEl} />
     </>
-  )
+  );
 }
