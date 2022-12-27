5c724417fea49be72e22cbccaee10f031a4fc02a
Jay Min <dj.min43@gmail.com>
2022-12-27 14:30:28 +0900

    use ref

--- fabricjs/pages/index.tsx
+++ fabricjs/pages/index.tsx
@@ -1,21 +1,34 @@
 import { useEffect, useRef } from "react";
 import { fabric } from "fabric";
 
-const kenImage =
-  "https://media.licdn.com/dms/image/D5612AQFImzcZb43-3g/article-cover_image-shrink_720_1280/0/1670831920407?e=2147483647&v=beta&t=LM6hVOXFdSyNK40isUdFj-Yi89xVM0j4g0ntNYG52oE";
-
 export default function Home() {
   const canvasEl = useRef(null);
-  useEffect(() => {
-    const canvas = new fabric.Canvas(canvasEl.current);
+  const canvas = useRef<fabric.Canvas | null>(null);
 
-    const someText = new fabric.Text("hello spring", {
-      fontSize: 40,
-    });
-    canvas.add(someText);
+  useEffect(() => {
+    if (canvasEl.current && !canvas.current) {
+      canvas.current = new fabric.Canvas(canvasEl.current);
+      canvas.current.isDrawingMode = true;
+      canvas.current.freeDrawingBrush.color = "red";
+      canvas.current.freeDrawingBrush.width = 1;
+    }
   }, []);
+
   return (
     <>
+      <label>brush width</label>
+      <button
+        onClick={() => {
+          if (canvas.current) {
+            canvas.current.freeDrawingBrush.width =
+              canvas.current.freeDrawingBrush.width + 1;
+            console.log(canvas.current.freeDrawingBrush.width);
+          }
+        }}
+      >
+        increase
+      </button>
+      <button>decrease</button>
       <canvas width={"300"} height={"300"} ref={canvasEl} />
     </>
   );
