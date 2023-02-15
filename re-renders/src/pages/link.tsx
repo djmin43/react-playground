66ad7c5e8acb2732904490ef0226b5e1595e14af
Jay Min <dj.min43@gmail.com>
2023-02-16 07:25:54 +0900

    trying out different nextjs render strategy

---
+++ re-renders/src/pages/link.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Link = () => {
+  return <div>this is link</div>;
+};
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+  };
+};
+
+export default Link;
---
+++ re-renders/src/pages/router.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Router = () => {
+  return <div>router</div>;
+};
+
+export default Router;
