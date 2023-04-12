943e66295730951db8384b292284d59051df0c9e
jay <dj.min43@gmail.com>
2023-04-12 20:14:41 +0900

    set up babel plugin

---
+++ hook-lib/rollup.config.js
@@ -0,0 +1,13 @@
+import { babel} from '@rollup/plugin-babel'
+
+const config =  {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+    },
+    external: [/@babel\/runtime/],
+    plugins: [babel({babelHelpers: 'runtime'})]
+}
+
+export default config;
