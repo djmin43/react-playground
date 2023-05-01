ab002421190d1305ff19b6b9bae1165f6ab24dc5
jay <dj.min43@gmail.com>
2023-05-01 14:37:00 +0900

    add providers

---
+++ next13-for-real/src/app/providers.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { CacheProvider } from "@emotion/react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
