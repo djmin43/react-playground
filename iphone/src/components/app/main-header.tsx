6e4bcbd67426597c4922098236d02a691008ba84
jay <dj.min43@gmail.com>
2023-06-04 18:53:22 +0900

    create main header

---
+++ iphone/src/components/app/main-header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+export const MainHeader = () => {
+  return (
+    <div className={"w-full flex justify-center bg-neutral-100"}>
+      <Image
+        src={"/logos/apple-logo.svg"}
+        alt={"apple logo"}
+        width={52}
+        height={52}
+      />
+    </div>
+  );
+};
---
+++ iphone/src/fonts/index.tsx
@@ -0,0 +1,5 @@
+import localFont from "@next/font/local";
+
+export const mainFont = localFont({
+  src: "./apple-sd-gothic/AppleSDGothicNeoR.ttf",
+});
