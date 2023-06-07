c1e9e21f3922fcdbe7188cc7fa02fc3439925145
James Min <james.min@freedgrouptech.com>
2023-06-08 07:39:21 +0900

    add image

--- iphone/src/components/app/hero.tsx
+++ iphone/src/components/app/hero.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className={"flex justify-center p-4"}>
+    <div className={"flex flex-col items-center p-4 gap-5"}>
       <p className={"text-4xl font-semibold"}>놀라움 한가득</p>
+      <Image
+        src={"/hero/hero-image.jpg"}
+        alt={"iphone"}
+        width={675}
+        height={342}
+      />
     </div>
   );
 };
