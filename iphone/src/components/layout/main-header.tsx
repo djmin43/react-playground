a8f08832657b0631b134477aaef2893a99325d2e
jay <dj.min43@gmail.com>
2023-06-11 12:56:36 +0900

    add link to route

--- iphone/src/components/layout/main-header.tsx
+++ iphone/src/components/layout/main-header.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const MainHeader = () => {
   return (
     <div className={"w-full flex justify-between bg-gray-100"}>
-      <Image
-        src={"/logos/apple-logo.svg"}
-        alt={"apple logo"}
-        width={52}
-        height={52}
-      />
+      <Link href={"/"}>
+        <Image
+          src={"/logos/apple-logo.svg"}
+          alt={"apple logo"}
+          width={52}
+          height={52}
+        />
+      </Link>
     </div>
   );
 };
