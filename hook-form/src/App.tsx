aecb8616228b5a8e94a7873db512d8d153969978
James Min <james.min@freedgrouptech.com>
2023-06-15 16:36:44 +0900

    existing form

--- hook-form/src/App.tsx
+++ hook-form/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Basic } from "./examples/basic.tsx";
 import { RegisterFields } from "./examples/register-fields.tsx";
+import { ExistingForm } from "./examples/existing-form.tsx";
 
 function App() {
   return (
     <main>
       <Basic />
       <RegisterFields />
+      <ExistingForm />
     </main>
   );
 }
