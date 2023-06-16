f2451442d3eb44682e0a8f3da614fecaf5dafa89
James Min <james.min@freedgrouptech.com>
2023-06-16 16:43:27 +0900

    reset

--- hook-form/src/App.tsx
+++ hook-form/src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import { Basic } from "./examples/basic.tsx";
-import { RegisterFields } from "./examples/register-fields.tsx";
-import { ExistingForm } from "./examples/existing-form.tsx";
-import { HandleErrors } from "./examples/handle-errors.tsx";
+import { PlayGround } from "./examples/play-ground.tsx";
 
 function App() {
   return (
     <main>
-      <Basic />
-      <RegisterFields />
-      <ExistingForm />
-      <HandleErrors />
+      <PlayGround />
     </main>
   );
 }
---
+++ hook-form/src/examples/play-ground.tsx
@@ -0,0 +1,7 @@
+export const PlayGround = () => {
+  return (
+    <div>
+      <h1>play ground</h1>
+    </div>
+  );
+};
