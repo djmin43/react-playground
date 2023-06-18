f38b28bb5c0ec6956e8795a0b6905e5be88d28e3
jay <dj.min43@gmail.com>
2023-06-18 12:54:51 +0900

    add images

--- iphone/src/components/product/models/model-image.ts
+++ iphone/src/components/product/models/model-image.ts
@@ -4,9 +4,20 @@ export const modelImage = {
   [product["14pro"]]: {
     [modelName["14pro"]]: {
       [iphoneColor.silver]: "/product/iphone/14-pro/iphone-14-pro-silver.jpeg",
+      [iphoneColor.deepPurple]:
+        "/product/iphone/14-pro/iphone-14-pro-silver.jpeg",
+      [iphoneColor.spaceBlack]:
+        "/product/iphone/14-pro/iphone-14-pro-silver.jpeg",
+      [iphoneColor.gold]: "/product/iphone/14-pro/iphone-14-pro-silver.jpeg",
     },
     [modelName["14proMax"]]: {
       [iphoneColor.silver]:
+        "/product/iphone/14-pro/iphone-14-pro-max-silver.jpeg",
+      [iphoneColor.deepPurple]:
+        "/product/iphone/14-pro/iphone-14-pro-max-silver.jpeg",
+      [iphoneColor.spaceBlack]:
+        "/product/iphone/14-pro/iphone-14-pro-max-silver.jpeg",
+      [iphoneColor.gold]:
         "/product/iphone/14-pro/iphone-14-pro-max-silver.jpeg",
     },
   },
