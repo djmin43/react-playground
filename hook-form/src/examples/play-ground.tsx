31e92a8664cfbe4ca899eb0503ecfb79d4e5db2a
jay <dj.min43@gmail.com>
2023-06-20 18:30:55 +0900

    use litera

--- hook-form/src/examples/play-ground.tsx
+++ hook-form/src/examples/play-ground.tsx
@@ -2,20 +2,20 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-const schema = z.object({
-  name: z
-    .string({
-      required_error: "name is required",
-      invalid_type_error: "your name is invalid!",
-    })
-    .max(5, "your name is too long!"),
-  phoneNumber: z.string({
-    required_error: "phone number is required",
-    description: "this is phone number",
-    invalid_type_error: "your phone number is invalid!",
-  }),
-  menu: z.string(),
-});
+const schema = z
+  .object({
+    name: z
+      .string()
+      .min(1, "please write user name")
+      .max(5, "your name is too long!"),
+    phoneNumber: z.string(),
+    email: z.string().email(),
+    menu: z.string(),
+    terms: z.literal(true, {
+      errorMap: () => ({ message: "please check the terms" }),
+    }),
+  })
+  .required();
 
 type Schema = z.infer<typeof schema>;
 
@@ -38,17 +38,22 @@ export const PlayGround = () => {
         <div>
           <label>name:</label>
           <input {...register("name")} />
-          {errors.name && errors.name?.message}
+          {errors.name?.message}
         </div>
         <div>
           <label>phone number:</label>
           <input {...register("phoneNumber")} />
-          {errors.phoneNumber && errors.phoneNumber?.message}
+          {errors.phoneNumber?.message}
         </div>
         <div>
           <label>menu:</label>
           <input {...register("menu")} />
           {errors.menu && errors.menu?.message}
+        </div>
+        <div>
+          <label>terms:</label>
+          <input {...register("terms")} type={"checkbox"} />
+          {errors.terms && errors.terms?.message}
         </div>
         <input type="submit" />
       </form>
