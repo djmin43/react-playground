fbcd742708e174b1170128282cfba1ba9a204bfa
jay <dj.min43@gmail.com>
2023-07-15 16:38:50 +0900

    database

--- test123/src/firebase/config.ts
+++ test123/src/firebase/config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDaWSJU-DRT-mq9EVW6xeS9puWBgZcURk4",
@@ -10,9 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "246089681773",
   appId: "1:246089681773:web:429905faead3926a242864",
   measurementId: "G-R50KQ4LD99",
+  databaseURL: "https://test123-32ade-default-rtdb.firebaseio.com",
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
+export const database = getDatabase(app);
 const analytics = getAnalytics(app);
