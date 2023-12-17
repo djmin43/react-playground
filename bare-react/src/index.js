bbeced2d9d3c8dc7472e3f1f14828da823d2714f
jay <dj.min43@gmail.com>
2023-12-17 15:21:31 +0900

    remove webpack

--- bare-react/src/index.js
+++ bare-react/src/index.js
@@ -2,4 +2,7 @@ import {createElement} from "react";
 const helloElement = createElement("h1", {className: "hello"}, "hello world")
 console.log(helloElement)
 
+
+// const el = document.getElementById("app");
+
 console.log("hello")
