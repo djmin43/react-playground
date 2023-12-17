4a2440e3fd0d319498bcd4d24c0254e74301f356
jay <dj.min43@gmail.com>
2023-12-17 16:49:01 +0900

    try with stateful component

--- bare-react/src/index.js
+++ bare-react/src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { createElement} from "react";
+import {createElement, useState} from "react";
 import {createRoot} from "react-dom";
 function component() {
     const element = document.createElement("div");
@@ -11,13 +11,26 @@ function component() {
 
 document.body.appendChild(component());
 
-const testComponent = createElement("h1", {}, "hello react")
+const StatefulComponent = () => {
+    const [count, useCount] = useState(0)
+    return (
+        <div>
+            {count}
+        </div>
+    );
+};
+console.log("stateful", StatefulComponent)
+
+const childComponent = createElement("span", {className: " child"}, "I am a child component")
+const testComponent = createElement("h1", {}, ["hello react", StatefulComponent])
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-root.render(testComponent);
+const render = root.render(testComponent);
+console.log("render", render)
 
 console.log(testComponent)
 
 console.log("hello world")
+
