cd5faa10cfdec9aa3064fc50d1def41d295b4dec
jay <dj.min43@gmail.com>
2023-12-17 16:26:14 +0900

    create hello react

--- bare-react/src/index.js
+++ bare-react/src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { createElement} from "react";
+import {createRoot} from "react-dom";
 function component() {
     const element = document.createElement("div");
 
@@ -11,6 +12,11 @@ function component() {
 document.body.appendChild(component());
 
 const testComponent = createElement("h1", {}, "hello react")
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(testComponent);
 
 console.log(testComponent)
 
