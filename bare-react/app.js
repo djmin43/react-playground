999f6e77c2af75c3db9c520653904c85385a1063
dongjoon min <dj.min43@gmail.com>
2023-12-29 18:53:59 +0900

    set count

--- bare-react/app.js
+++ bare-react/app.js
@@ -1,8 +1,21 @@
-const countElement = document.getElementById("count");
+
+const countApp = {
+    getCount: () => {
+        const countElement = document.getElementById("count");
+        return Number(countElement.textContent);
+    },
+    setCount: (val) => {
+        const countElement = document.getElementById("count");
+        countElement.textContent = val;
+
+    },
+}
 
 function setCount() {
-    let count = Number(countElement.textContent);
-    count = count + 1;
-    countElement.textContent = count;
-
+    let count = countApp.getCount();
+    if (count >= 5) {
+        countApp.setCount(0);
+    } else {
+        countApp.setCount(count + 1);
+    }
 }
