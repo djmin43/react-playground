9e6de267f7f2d75176dcb7cec55cf7ddc7727020
dongjoon min <dj.min43@gmail.com>
2023-12-29 19:02:10 +0900

    fake tree

--- bare-react/app.js
+++ bare-react/app.js
@@ -1,21 +1,14 @@
-
-const countApp = {
-    getCount: () => {
-        const countElement = document.getElementById("count");
-        return Number(countElement.textContent);
-    },
-    setCount: (val) => {
-        const countElement = document.getElementById("count");
-        countElement.textContent = val;
-
-    },
-}
-
-function setCount() {
-    let count = countApp.getCount();
-    if (count >= 5) {
-        countApp.setCount(0);
-    } else {
-        countApp.setCount(count + 1);
-    }
+let markup = {
+    type: 'article',
+    children: [
+        {
+            type: 'h2',
+            children: [
+                {
+                    type: "text",
+                    value: "Counter",
+                }
+            ],
+        }
+    ],
 }
