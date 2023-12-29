e62ec2f2db4451c23b68234e24c6921520281323
dongjoon min <dj.min43@gmail.com>
2023-12-29 19:27:39 +0900

    create a button element with react create elemenet

--- bare-react/app.js
+++ bare-react/app.js
@@ -1,3 +1,14 @@
 const rootNode = document.getElementById("app");
 
-console.log(React);
+// Where it should start
+const root = ReactDOM.createRoot(rootNode);
+
+root.render(React.createElement(App));
+
+function App() {
+    console.log("Called App")
+
+    return React.createElement("button", null, "Click me");
+}
+
+console.log(App());
