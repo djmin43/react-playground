f269df6544ee3ac842f36b31f7d284103c5c51b5
dongjoon min <dj.min43@gmail.com>
2023-12-29 18:41:09 +0900

    counter

---
+++ bare-react/index.js
@@ -0,0 +1,8 @@
+const countElement = document.getElementById("count");
+
+function setCount() {
+    let count = Number(countElement.textContent);
+    count = count + 1;
+    countElement.textContent = count;
+
+}
