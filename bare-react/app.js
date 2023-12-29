8d522832d254c4c9a7114a89c146578bb200d545
dongjoon min <dj.min43@gmail.com>
2023-12-29 19:17:22 +0900

    console log react

--- bare-react/app.js
+++ bare-react/app.js
@@ -1,14 +1,3 @@
-let markup = {
-    type: 'article',
-    children: [
-        {
-            type: 'h2',
-            children: [
-                {
-                    type: "text",
-                    value: "Counter",
-                }
-            ],
-        }
-    ],
-}
+const rootNode = document.getElementById("app");
+
+console.log(React);
