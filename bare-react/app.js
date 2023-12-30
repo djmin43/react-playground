fc89cc7dfe3b24e2f0dbb66f4c0a1b333829a761
dongjoon min <dj.min43@gmail.com>
2023-12-30 11:52:18 +0900

    use local react code

--- bare-react/app.js
+++ bare-react/app.js
@@ -1,9 +1,7 @@
 // This is just a DOM object. variable is pointing to the object's reference.
 const rootNode = document.getElementById("app");
-
 // Where it should start
 const root = ReactDOM.createRoot(rootNode);
-
 root.render(React.createElement(App));
 
 function App() {
@@ -14,20 +12,3 @@ function App() {
             React.createElement("button", null, "Click me"))
     );
 }
-
-console.log(App());
-
-// before React does its work
-let articleElements = document.getElementsByTagName("article");
-let articleElement = document.getElementsByTagName("article").item(0);
-console.log(articleElements);
-console.log(articleElement);
-
-
-// after React does its work
-setTimeout(function () {
-    let articleElements = document.getElementsByTagName("article");
-    let articleElement = document.getElementsByTagName("article").item(0);
-    console.log(articleElements);
-    console.log(articleElement);
-}, 1000);
