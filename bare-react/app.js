19555e17cb34eecbffa4aeace9e5db3bb8b00f6a
dongjoon min <dj.min43@gmail.com>
2024-01-01 15:24:08 +0900

    try use state

--- bare-react/app.js
+++ bare-react/app.js
@@ -6,63 +6,30 @@ root.render(<App />);
 let counterName = "One"
 
 function App() {
-    const counterOne = <Counter name={counterName} />
-    const counterTwo = <Counter2 name={counterName} />
     return <section>
         <h1>Counters</h1>
         <section>
-            {counterName === "One" ? counterOne : counterTwo}
+            <Counter name="One" />
         </section>
     </section>;
 }
 
-function Counter({name}) {
+function Counter(props) {
 
-    // Synthetic event
-    const clickHandler = (event) => {
-        console.log("React handled click");
-        console.log(event);
-    }
 
-    const parentClickHandler = (event) => {
-        console.log("Parent was clicked too");
-    }
+    const [numOfClicks, setNumOfClicks] = React.useState(0);
 
-    const linkClickHandler = (event) => {
-        console.log("Going to site");
-    }
-
-    return (
-        <article onClick={parentClickHandler}>
-            <h2>Counter {name}</h2>
-            <p>You clicked 1 times</p>
-            <button className="button" onClick={clickHandler}>
-                Click me
-            </button>
-            <p>
-                <a href="http://understandingreact.com" target="_blank" onClick={linkClickHandler}>Understanding React</a>
-            </p>
-        </article>
-    );
-}
-
-function Counter2({name}) {
     return (
         <article>
-            <h2>Counter {name}</h2>
-            <p>You clicked 1 times</p>
-            <button className="button">
+            <h2>Counter {props.name}</h2>
+            <p>You clicked {numOfClicks} times</p>
+            <button className="button" onClick={() => setNumOfClicks(numOfClicks + 1)}>
                 Click me
             </button>
         </article>
     );
 }
 
-function rerender() {
-    console.log("Rerender...");
-    counterName = "Two";
-    root.render(React.createElement(App));
-}
 
 
 
