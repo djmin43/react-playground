e96438e4294b33b1d3f1e72d72793e1ae4f02442
dongjoon min <dj.min43@gmail.com>
2024-04-21 21:28:26 +0900

    actually linking the datbase to the UI

--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,31 +1,18 @@
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
-
-const mockUrls = [
-  "https://utfs.io/f/ce1dacf1-b158-4dfc-8b05-e27eaf215432-r2h838.jpg",
-  "https://utfs.io/f/f3d0deec-31ea-46f6-b17e-3ab06e375fd0-nfpovb.jpg",
-  "https://utfs.io/f/cb9c041a-4d76-434b-bd4d-547b9d0b130e-nk6w98.jpg",
-  "https://utfs.io/f/25c6b222-3990-4d68-88f7-870716a1b9cc-ckhus1.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => asc(model.name),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="w-48">
-            <img src={image.url} alt="image" />
+            <img src={image.url} alt={image.name} />
+            <span>{image.name}</span>
           </div>
         ))}
       </div>
