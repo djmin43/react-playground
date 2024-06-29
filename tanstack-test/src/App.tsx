626d7a525c75c1841a8a682847afc358cc74f245
dongjoon min <dj.min43@gmail.com>
2024-06-29 14:02:54 +0900

    remove tanstack test

--- tanstack-test/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <>hh</>;
-}
-
-export default App;
--- tanstack-test/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), viteReact()],
-});
