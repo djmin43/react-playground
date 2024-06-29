121b615362f670a44b8ef89ef1fff49f09f4cee4
dongjoon min <dj.min43@gmail.com>
2024-06-29 13:45:36 +0900

    add router

--- tanstack-test/vite.config.ts
+++ tanstack-test/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), TanStackRouterVite(), viteReact()],
+});
