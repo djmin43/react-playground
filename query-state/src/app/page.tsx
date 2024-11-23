fb112c8c791a596383c67b8714afdc2d22b66e95
dongjoon min <dj.min43@gmail.com>
2024-11-23 18:37:57 +0900

    basic query state done

---
+++ query-state/src/app/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import {useEffect, useState} from "react";
+
+export default function Home() {
+    const [query, setQuery] = useQueryState("test")
+
+    const [inputValue, setInputValue] = useState("")
+
+    useEffect(() => {
+        console.log(query)
+
+    }, [query])
+
+    return (
+        <div>
+            <div>hello world</div>
+            <span>
+                {query}
+            </span>
+                <input type="text" value={inputValue} onChange={(e) => {
+                    setInputValue(e.target.value)
+                    setQuery(e.target.value)
+                }}/>
+
+        </div>
+    );
+}
+
+const useQueryState = (key: string): [string | null, (value: string) => void] => {
+
+    const getQueryParamsValue = (key: string) => {
+        const queryParams = new URLSearchParams(window.location.search)
+        return queryParams.get(key)
+    }
+
+    const [queryValue, setQueryValue] = useState<string>(getQueryParamsValue(key) ?? "")
+
+    const handleSetQueryValue = (value: string) => {
+        const queryParams = new URLSearchParams(window.location.search)
+        queryParams.set(key, value)
+        const encodedQuery = queryParams.toString().replace(/\+/g, '%20');
+
+        window.history.pushState({}, '', `${window.location.pathname}?${encodedQuery}`);
+        setQueryValue(value)
+    }
+
+
+    return [queryValue, handleSetQueryValue]
+
+}
